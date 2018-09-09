0396fb1d33a2094890f87e67ff355d0c8cd8c41b
Paula George <polagorge@gmail.com>
2018-09-09 14:40:18 -0700

    finished node first impl

---
+++ node.php
@@ -0,0 +1,15 @@
+<?php
+    class node{
+        public $next = null ;
+        public $value ;
+
+        public function __construct($val,$n)
+        {
+            $this->next = $n ;
+            $this->value ;
+        }
+
+    }
+
+
+?>
