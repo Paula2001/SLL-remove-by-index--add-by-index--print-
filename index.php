3815c07a062857ae8048936365932d1324413a20
Paula George <polagorge@gmail.com>
2018-09-11 10:00:38 -0700

    finished remove method

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
     $s1->_add(1,0);
     $s1->_add(2,1);
     $s1->_add(3,2);
+    $s1->_add(4,1);
+    $s1->_add(6,0);
+    $s1->_print();
+    echo  "print after remove"."<br>" ;
+    $s1->_remove(4);
 
     $s1->_print();
 
--- linkedlist.php
+++ linkedlist.php
@@ -2,34 +2,51 @@
     include_once "node.php";
 ?>
 <?php
-    class Linkedlist  {
-        private $tail = null ;
+    class Linkedlist
+    {
+        private $tail = null;
 
-        public function _add($val,$index){
+        public function _add($val, $index)
+        {
             $newNode = new Node($val);
-            if($index == 0 ){
-                $newNode->next = $this->tail ;
+            if ($index == 0) {
+                $newNode->next = $this->tail;
                 $this->tail = $newNode;
-            }else {
+            } else {
                 $i = $this->tail;
-                for($count = 0;$count < $index - 1;$count++){
-                    $i = $i->next ;
+                for ($count = 0; $count < $index - 1; $count++) {
+                    $i = $i->next;
                 }
-                $newNode->next = $i->next ;
-                $i->next = $newNode ;
-
-            }
-
-
-        }
-        public function _print(){
-            $print_v = $this->tail ;
-            while($print_v != null){
-                echo $print_v->value ;
-                $print_v = $print_v->next ;
+                $newNode->next = $i->next;
+                $i->next = $newNode;
             }
         }
 
+        public function _print()
+        {
+            $i = $this->tail;
+            while ($i != null) {
+                echo $i->value;
+                echo '<br>' ;
+                $i = $i->next;
+            }
+        }
+
+        public function _remove($index)
+        {
+            $i = $this->tail;
+            $x = $this->tail;
+            if ($index == 0) {
+                $this->tail = $i->next;
+            } else {
+                for ($count = 0; $count < $index - 1; $count++) {
+                    $i = $i->next;
+                }
+
+                $x = $i->next;
+                $i->next = $x->next;
+            }
+
+        }
     }
-
 ?>
